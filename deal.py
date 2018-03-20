f9de293db2bcbd708ddd2f15d2585c04e497a324
Wanghaohun <437642013@qq.com>
2018-03-20 16:03:43 +0800

    feature http

--- deal.py
+++ deal.py
@@ -1,7 +1,33 @@
 import json
 import os
 import re
+import test2
+import urllib.parse
+import urllib.request
+###get
+response = urllib.request.urlopen('http://192.168.31.101:8080/task/ei/get')
+html = response.read()  
+print(html.decode('utf-8'))
+###  
 
+###post
+url = 'http://127.0.0.1:8080/test/index.jsp'  
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' 
+headers = {'Content-Type': 'application/json'} 
+values = {  
+    'name': 'abc',  
+    'password': '123'  
+}  
+data = urllib.parse.urlencode(values)  
+# that params output from urlencode is encoded to bytes before it is sent to urlopen as data  
+data = data.encode('utf-8')  
+req = urllib.request.Request(url, data)  
+response = urllib.request.urlopen(req)  
+  
+html = response.read()  
+print(html.decode('utf-8'))  
+###
+test2.search()
 f=open(r"C:\Users\Administrator\EV_download\Engineering_Village_detailed_3-19-2018_5272473.txt","r")
 lines=f.readlines()
 dict={}
