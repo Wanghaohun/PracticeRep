c868888b606300de6a9548fd02dad136c5b1890f
Wanghaohun <437642013@qq.com>
2018-03-20 12:01:35 +0800

    loop completed

--- deal.py
+++ deal.py
@@ -1,18 +1,21 @@
 import json
 import os
 import re
+
 f=open(r"C:\Users\Administrator\EV_download\Engineering_Village_detailed_3-19-2018_5272473.txt","r")
 lines=f.readlines()
 dict={}
 dict2={}
 dict2['taskid']='asdsafasasfsafsafa'
 record=1
+list=[]
 for count in range(len(lines)):
-    if record>4:
+    if record>2:
         break
     if re.match('<RECORD %d>'%(record+1),lines[count]):
         record=record+1
         list.append(dict)
+        dict={}
     if lines[count]=='\n':
         continue
     if re.match('<RECORD %d>'%record,lines[count]):
